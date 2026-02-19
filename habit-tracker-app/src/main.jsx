58fb50ef35cad73d6529b4ebf927b82c823983f4
elgin02 <100308264+elgin02@users.noreply.github.com>
2026-02-19 10:42:12 -0600

    Reverted main.jsx

--- habit-tracker-app/src/main.jsx
+++ habit-tracker-app/src/main.jsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Login from './Login.jsx'
-//import App from './App.jsx'
+//import Login from './Login.jsx'
+import App from './App.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Login /> 
+    {*/ <Login /> /*}
+  <App /> 
   </StrictMode>
 )
